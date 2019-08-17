fd8a3001cfbd6225aa36cc714a74986d540d2937
Gabriel Correa <correa.gabrielsilva@outlook.com>
2019-08-17 11:34:24 -0300

    Estou subindo a nova linha

--- prj_salvando_no_git/Program.cs
+++ prj_salvando_no_git/Program.cs
@@ -7,7 +7,13 @@ namespace prj_salvando_no_git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Aula de Git de GitHub");
+
+            Console.WriteLine("Aula de Git de GitHub!");
+
+            Console.WriteLine("Preparando para subir no GitHub.");
+
+            Console.Write("Digite seu Nome: ");
+            string nome = Console.ReadLine();
         }
     }
 }
