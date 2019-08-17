234bf88632ecabe0eec9d7ae1b2dc81f8c2ca47f
Gabriel Correa <correa.gabrielsilva@outlook.com>
2019-08-17 10:06:59 -0300

    salvando o projeto_solution

---
+++ prj_salvando_no_git/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace prj_salvando_no_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Aula de Git de GitHub");
+        }
+    }
+}
